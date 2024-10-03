75c3fe14468117a6b213c75817a2ce52791674d7
Monsef Rachid <monsef.r@gmail.com>
2024-10-03 04:07:22 -0400

    Added exception handling to API module

--- api.py
+++ api.py
@@ -11,18 +11,23 @@ headers = {
 def fetch_scraping_list():
     endpoint = f"{config.API_BASE_URL}/alert/list"
     logger.info(f"Fetching latest alerts: GET {endpoint}")
-    response = requests.get(endpoint, headers=headers)
-    if response.status_code == 200:
+    
+    try:
+        response = requests.get(endpoint, headers=headers)
+        response.raise_for_status()  # Raises an HTTPError if the status is 4xx, 5xx
         logger.info('Fetch successful')
         return response.json().get('data', {})
-    logger.error(f"Failed to retrieve data. Status code: {response.status_code}. Message: {response.json().get('message')}", exc_info=True)
-    return None
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Failed to retrieve data. Exception: {str(e)}", exc_info=True)
+        return None
 
 def post_result(post_data):
     endpoint = f"{config.API_BASE_URL}/post"
     logger.info(f"Add Post: POST {endpoint}", extra={'json_data': post_data})
-    response = requests.post(endpoint, json=post_data, headers=headers)
-    if response.status_code == 200 or response.status_code == 201:
+
+    try:
+        response = requests.post(endpoint, json=post_data, headers=headers)
+        response.raise_for_status()
         logger.info(response.json().get('message'))
-    else:
+    except requests.exceptions.RequestException as e:
         logger.error(f"Failed to create post\nStatus code: {response.status_code}\nMessage: {response.json().get('message')}", exc_info=True)
